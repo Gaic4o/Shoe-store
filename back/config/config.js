90451691b083464c3628bdfa0124d59d776ff672
김민수 <alstntorl@naver.com>
2021-05-24 09:56:46 +0900

    Update config.js

--- back/config/config.js
+++ back/config/config.js
@@ -21,7 +21,7 @@ module.exports = {
   production: {
     username: 'root',
     password:  process.env.DB_PASSWORD,
-    database: 'Dhtmxm@1',
+    database: '',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
